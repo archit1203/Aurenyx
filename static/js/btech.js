820a6bb1b024abaa59f6b8e2c1adc1e556e66fc3
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2025-06-07 04:04:14 +0530

    Update btech.js

--- static/js/btech.js
+++ static/js/btech.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { 
   const loadBtn = document.getElementById('loadProjectsBtn');
   const projectList = document.getElementById('projectsList');
 
